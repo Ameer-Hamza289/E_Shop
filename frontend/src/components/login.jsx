b87fbbe143d213e72f02a6987df05550ee502cd4
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-11-10 09:53:00 +0500

    Basic Setup

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Login = () => {
+  return (
+    <div className='min-h-screen bg-grey-50 flex flex-col justify-center py-12 sm:px-6'>
+        login
+    </div>
+  )
+}
+
+export default Login
