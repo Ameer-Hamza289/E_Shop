ab327b268ecb46fa91c630ff54cad1b9f5326650
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-12-26 10:44:51 +0500

    Socket Setup in progress

--- backend/controller/conversationController.js
+++ backend/controller/conversationController.js
@@ -2,18 +2,86 @@ const express=require("express")
 const router=express.Router();
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ErrorHandler = require("../utils/ErrorHandler");
+const Conversation = require("../model/conversation");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const { isSeller, isAuthenticated } = require("../middleware/auth");
 
-
-
-router.post("/",catchAsyncErrors(async(req,res,next)=>{
+router.post("/create-new-conversation",catchAsyncErrors(async(req,res,next)=>{
     try {
-        
+        const { groupTitle, userId, sellerId } = req.body
+        const isConversationExist = await Conversation.findOne({ groupTitle });
+        if (isConversationExist) {
+            const conversation = isConversationExist;
+            res.status(201).json({
+              success: true,
+              conversation,
+            });
+          } else{
+            const conversation = await Conversation.create({
+                members: [userId, sellerId],
+                groupTitle: groupTitle,
+              });
+      
+              res.status(201).json({
+                success: true,
+                conversation,
+              });
+          }
     } catch (error) {
         return next(new ErrorHandler(error.message, 500));   
     }
 }))
 
+router.post("/get-all-conversation-user/:id",isAuthenticated,catchAsyncErrors(async(req,res,next)=>{
+    try {
+        const conversations = await Conversation.find({
+            members: {
+              $in: [req.params.id],
+            },
+          }).sort({ updatedAt: -1, createdAt: -1 });
+    
+          res.status(201).json({
+            success: true,
+            conversations,
+          })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));   
+    }
+}))
 
+router.get("/get-all-conversation-seller/:id", isSeller,catchAsyncErrors(async(req,res,next)=>{
+    try {
+        const conversations = await Conversation.find({
+            members: {
+              $in: [req.params.id],
+            },
+          }).sort({ updatedAt: -1, createdAt: -1 });
+    
+          res.status(201).json({
+            success: true,
+            conversations,
+          })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));   
+    }
+}))
 
+router.post("/update-last-message/:id",catchAsyncErrors(async(req,res,next)=>{
+    try {
+        const { lastMessage, lastMessageId } = req.body;
+
+        const conversation = await Conversation.findByIdAndUpdate(req.params.id, {
+          lastMessage,
+          lastMessageId,
+        });
+  
+        res.status(201).json({
+          success: true,
+          conversation,
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));   
+    }
+}))
 
 module.exports=router
---
+++ socket/index.js
@@ -0,0 +1,19 @@
+const socketIO = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world from socket server!");
+  });
+
+  server.listen(process.env.PORT || 4000, () => {
+    console.log(`server is running on port http://localhost:${process.env.PORT || 4000}`);
+  });
